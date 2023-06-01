13b6207f527dc4a8223d9816d86c60f70c38bd65
Atefnouri <nouriatef.isa@gmail.com>
2023-06-01 17:53:52 +0100

    add new tasks to the list

--- main.js
+++ main.js
@@ -1,11 +1,49 @@
 var TodoList = /** @class */ (function () {
     function TodoList() {
+        var _this = this;
+        this.result = "";
         this.addToList = function (message) {
+            _this.result = '';
             console.log("this a ".concat(message));
+            _this.todos.push({
+                id: _this.todos.length + 1,
+                task: message,
+                completed: false,
+            });
+            console.table(_this.todos);
+            _this.disPlayHandler();
+        };
+        this.disPlayHandler = function () {
+            console.log('disPlayHandler');
+            _this.todos.forEach(function (el) {
+                var item = "<li>".concat(el.task, "</li> \n");
+                _this.result += (item);
+            });
+            console.log('result', _this.result);
+            var taskArea = document.querySelector('#listdiv');
+            if (taskArea) {
+                taskArea.innerHTML = _TodoList.result;
+            }
         };
         this.todos = [];
-        alert("test");
+        console.log('App is running ');
     }
     return TodoList;
 }());
+//main function
 var _TodoList = new TodoList();
+var button = document.querySelector('#addButton');
+var input = document.querySelector('#task');
+var all = '';
+var displayTask = function (tab) {
+    tab.forEach(function (element) {
+        all.concat(element.task);
+    });
+    console.log('all', all);
+};
+if (button) {
+    button.addEventListener('click', function (event) {
+        event.preventDefault();
+        _TodoList.addToList(input.value);
+    });
+}
--- main.ts
+++ main.ts
@@ -5,22 +5,77 @@ interface Todo {
 }
 
 class TodoList {
-    private todos: Todo[];
+    public todos: Todo[];
+    public result:string = "";
   
     constructor() {
       this.todos = [];
-        
-      alert("test");
-    
+    console.log('App is running ');
     }
 
 
      addToList = (message:string) =>  {
-
+      this.result = '';
       console.log(`this a ${message}`);
-  
+
+      this.todos.push({
+              id: this.todos.length + 1,
+              task: message,
+              completed: false,
+            });
+
+            console.table(this.todos); 
+            this.disPlayHandler(); 
     }
+
+
+    disPlayHandler = () => {
+      console.log('disPlayHandler');
+      this.todos.forEach(el => {
+        let item:string = `<li>${el.task}</li> \n`;
+        this.result += (item);
+      });
+      console.log('result',this.result);
+      let taskArea = document.querySelector('#listdiv');
+      if(taskArea){
+        taskArea.innerHTML = _TodoList.result;
+        }
+    }
+
+   
 
   }
 
- const _TodoList = new TodoList(); 
+
+//main function
+ const _TodoList = new TodoList();
+
+ const button = document.querySelector('#addButton');
+ const input = document.querySelector('#task') as HTMLInputElement;
+
+ 
+var all:string = '';
+ const displayTask = (tab:Todo[]):void => {
+  tab.forEach(element => {
+    all.concat(element.task);
+  });
+  console.log('all',all);
+ }
+
+
+ if (button) {
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    _TodoList.addToList(input.value);
+  });
+
+  
+
+ }
+
+
+
+
+
+
+
